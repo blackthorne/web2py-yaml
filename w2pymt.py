85ddc185dcf639c2a3a8c038b3d87255cfc2661b
Francisco G. T. Ribeiro <blackthorne@ironik.org>
2010-04-07 04:34:53 +0100

    Model.set_default(), defines tables

--- w2pymt.py
+++ w2pymt.py
@@ -2,24 +2,27 @@
 # encoding: utf-8
 # Web2py YAML Model Translator
 
-import yaml,sys,#headers
+import yaml,sys #headers
 
 ########################################################################
 class Table:
 	"""represents a table that may be part of a Model"""
 	#----------------------------------------------------------------------
-	def __init__(self, name, fields={}, constraints={}):
+	def __init__(self, name, fields={}, constraints=[]):
 		"""Constructor"""
 		self.name, self.fields, self.constraints = name, fields, constraints
 		
 	def gen_code(self, dbname):
 		code = "%s.define_table(\'%s\'\n" % (dbname, self.name)
 		for i,field in enumerate(self.fields):
-			code += "SQLField(\'%s\', \'%s\')" % (field, self.fields[field])
-			if i +1 != len(self.fields):
-				code += ','
-			code += '\n'
-		code += ')'
+			if field == 'constraints':
+				self.constraints.append(self.fields[field])
+			else:
+				code += "\tSQLField(\'%s\', \'%s\')" % (field, self.fields[field]) # TODO: fix requires/default bug 
+				if i +1 != len(self.fields):
+					code += ','
+				code += '\n'
+		code += ')\n'
 		return code 
 	
 ########################################################################
@@ -27,20 +30,23 @@ class Model:
 	"""defines a Model to be translated"""
 
 	#----------------------------------------------------------------------
-	def __init__(self,
-	             name, 
-	             dbms = 'sqlite',
-	             dbfile = 'storage.sqlite',
-	             username = '', 
-	             password = '',
-	             hostname = 'localhost',
-	             parameters = '',
-	             options = []
-	     ):
+	def __init__(self, yaml_filename):
 		"""Constructor"""
-		self.name, self.dbms, self.dbfile, self.username, self.password = name, dbms, dbfile, username, password
-		self.hostname, self.parameters, self.options = hostname, parameters, options
-	
+		self.conf, self.tables = self.load_yaml_file(yaml_filename), []
+		for table in self.conf:
+			if table != 'db':
+				self.tables.append(Table(table, self.conf[table]))
+		for table in self.tables:                         # remove ?
+			del self.conf[table.name]
+		for key,default in [('dbms','sqlite'),('username',''),('password',''),('hostname','localhost'),('parameters',''), ('options',[])]:
+			self.set_default(key,default)
+		for table in self.tables:
+			print table.gen_code(self.conf['db']['name'])
+		
+	def set_default(self, conf_key, default_value):
+		if not self.conf['db'].has_key(conf_key):
+			self.conf['db'][conf_key] = default_value
+			
 	def load_yaml_file(self, filename):
 		file = open(filename)
 		dataMap = yaml.load(file)
@@ -48,7 +54,8 @@ class Model:
 		return dataMap
 		
 def main():
-	a = Table('asd')
+	print 'Loading model...'
+	model = Model('models/yaml/test.yml')
 	
 if __name__ == '__main__':
 	main()
