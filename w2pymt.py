ad8e376741b53137b4b855eeed59183dddf1feff
Francisco Gama T. R <blackthorne@ironik.org>
2011-01-27 15:38:45 +0000

    YAML tables order is now respected in generated model, small bug fixes

--- w2pymt.py
+++ w2pymt.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 # Web2py YAML Model Translator
 
-import yaml,sys,headers
-
+import yaml,sys,headers,re
 ########################################################################
 class Table:
 	"""represents a table that may be part of a Model"""
@@ -21,8 +20,17 @@ class Table:
 						code += "%s.%s.%s.%s=%s\n" % (dbname, self.name, field, key, self.fields[field][key])
 		return code
 	
+	#def gen_constraints2(self, dbname):
+		#code = ''
+		#for field in self.fields:
+			#if field.__class__() == {}:
+				#for key in field:
+					#if key != 'type':
+						#code += "%s.%s.%s.%s=%s\n" % (dbname, self.name, field, key, field[key])
+		#return code	
+	
 	def gen_code(self, dbname):
-		code = "%s.define_table(\'%s\'\n" % (dbname, self.name)
+		code = "%s.define_table(\'%s\',\n" % (dbname, self.name)
 		for i,field in enumerate(self.fields):
 			if self.fields[field] .__class__() == {}:
 				code += "\tSQLField(\'%s\', \'%s\')" % (field, self.fields[field]['type'])
@@ -40,15 +48,49 @@ class Table:
 			code += constraints + '\n'
 		return code 
 	
+	#def gen_code2(self, dbname):
+		#code = "%s.define_table(\'%s\',\n" % (dbname, self.name)
+		#for field in self.fields:
+			#if field.__class__() == {}:
+				#code += "\tSQLField(\'%s\', \'%s\')" % (field, field['type'])
+			#elif ',' in field:
+				#commaindex=field.find(',')
+				#code += "\tSQLField(\'%s\', \'%s\',%s)" % (field, field[:commaindex], field[commaindex+1:])
+			#else:
+				#code += "\tSQLField(\'%s\', \'%s\')" % (field, field) 
+			#if i +1 != len(self.fields):
+				#code += ','
+			#code += '\n'
+		#code += ')\n'
+		#constraints = self.gen_constraints(dbname)
+		#if constraints != '':
+			#code += constraints + '\n'
+		#return code 
+	
 ########################################################################
 class Model:
 	"""defines a Model to be translated"""
+	def load_w2pm_file(filename):
+		dataMap = []
+		file = open(filename)
+		dataMap.append(file.readlines())
+		print dataMap
+#		(\w*).define_table\('(\w*)',	
+	
 	def gen_db_def(self):
 		if self.conf['db']['parameters'] == '':
 			return "%s = DAL(\'%s\')\n" % (self.conf['db']['name'], self.conf['db']['dbline'])
 		else:
 			return "%s = DAL(\'%s\', %s)\n" % (self.conf['db']['name'], self.conf['db']['dbline'], self.conf['db']['parameters'])
-	
+		
+	def gen_tables2(self,dataMap, tables = []):
+		tables = [Table(table, dataMap[table]) for table in self.table_names]
+		for index, table in enumerate(self.table_names):
+			if dataMap[table].has_key('constraints'): 
+				tables[index].contraints = dataMap[table]['contraints']
+				del tables[index].fields['contraints']
+		return tables
+		
 	def gen_tables(self, dataMap, tables = {}):
 		for table in dataMap:
 			if table != 'db':
@@ -60,7 +102,7 @@ class Model:
 		return tables
 	
 	def gen_conf(self, dataMap):
-		for table in self.tables:                         # not really needed
+		for table in self.table_names:                         # not really needed
 			del dataMap[table]                       #
 		for key,default in self.defaults:
 			if not dataMap['db'].has_key(key):
@@ -70,23 +112,28 @@ class Model:
 				dataMap['db']['dbline'] = "sqlite://" + dataMap['db']['dbfile'] 
 			else:
 				dataMap['db']['dbline'] = "%s://%s:%s@%s/%s" % (dataMap['db']['dbms'], dataMap['db']['username'], dataMap['db']['password'], dataMap['db']['hostname'], dataMap['db']['name'])		
-		options = dataMap['db']['options']                                    #
+		options = dataMap['db']['options']
 		dataMap['db']['options'] = []
-		if ',' in options:#
-			for option in options.split(','):                                            # not really needed
-				dataMap['db']['options'].append(option.strip())      #
+		if ',' in options:
+			for option in options.split(','):
+				dataMap['db']['options'].append(option.strip())
 		else:
 			dataMap['db']['options'] = [options]
 		return dataMap
 	
 	#----------------------------------------------------------------------
 	def __init__(self, 
-	             yaml_filename, 
-	             defaults = [('dbms','sqlite'),('username',''),('password',''),('hostname','localhost'),('parameters',''), ('options',['all']), ('dbline',''), ('dbfile', 'storage.sqlite')]):
+	             filename, 
+	             defaults = [('dbms','sqlite'),('username',''),('password',''),('hostname','localhost'),('parameters',''), ('options',['all']), ('dbline',''), ('dbfile', 'storage.sqlite')],
+	             sourcetype='yaml'):
 		"""Constructor"""
-		dataMap = self.load_yaml_file(yaml_filename)                                 # 
-		self.defaults, self.tables = defaults, self.gen_tables(dataMap)        # these lines order should be respected
-		self.conf = self.gen_conf(dataMap)                                                      #
+		if sourcetype != 'yaml':
+			dataMap = self.load_w2pm_file(w2pm_filename)
+		else:
+			self.table_names = self.load_table_names(filename)
+			dataMap = self.load_yaml_file(filename)                                 # 
+			self.defaults, self.tables = defaults, self.gen_tables2(dataMap)        # these lines order should be respected
+			self.conf = self.gen_conf(dataMap)                                                      #
 			
 	def load_yaml_file(self, filename):
 		file = open(filename)
@@ -94,10 +141,25 @@ class Model:
 		file.close()
 		return dataMap
 
+	def load_table_names(self, filename):
+		file = open(filename)
+		p = re.compile('^(?!db:)(\w+):')
+		results = [line[:-2] for line in file.readlines() if p.findall(line) != []]
+		file.close()
+		return results
+
+		
 	def gen_code(self):
 		code = headers.gen_template(self.conf['db']['name'], self.gen_db_def(), self.conf['db']['options'])
 		for table in self.tables:
 			code += self.tables[table].gen_code(self.conf['db']['name'])
+		return code
+	
+	
+	def gen_code2(self):
+		code = headers.gen_template(self.conf['db']['name'], self.gen_db_def(), self.conf['db']['options'])
+		for table in self.tables:
+			code += table.gen_code(self.conf['db']['name'])
 		return code
 	
 	def __str__(self):
@@ -110,14 +172,20 @@ def usage():
 	exit()
 	
 def main():
-	if len(sys.argv) == 3:
-		print 'Loading model...'
+	if len(sys.argv) in [2,3]:
+		if len(sys.argv) == 3:
+			print 'Loading model...'
 		model = Model(sys.argv[1])
-		file = open(sys.argv[2],'w')
-		print 'Generating code to output file...'
-		file.write(model.gen_code())
+		if len(sys.argv) == 2:
+			file = sys.stdout
+		else:
+			file = open(sys.argv[2],'w')
+		if len(sys.argv) == 3:
+			print 'Generating code to output file...'
+		file.write(model.gen_code2())
 		file.close()
-		print 'done'
+		if len(sys.argv) == 3:
+			print 'done'
 	else:
 		usage()
 if __name__ == '__main__':
